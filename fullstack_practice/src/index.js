ac4b170cc2efeb73aba58a1c7f3df4f248f907b4
niranjanrao1509 <niranjanrao1509@gmail.com>
2020-04-29 05:50:22 +0400

    Object spread syntax

--- fullstack_practice/src/index.js
+++ fullstack_practice/src/index.js
@@ -1,60 +1,97 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+//PART 1.C
+// const Hello = (props) => {
+//   const dob = () => new Date().getFullYear() - props.age
+//   return(
+//   <div>
+//     <p>Hello {props.name} you are {props.age} old, you must have been born in {dob()}</p>
+//   </div>
+//   )
+// }
 
-const Hello = (props) => {
-  const dob = () => new Date().getFullYear() - props.age
-  return(
-  <div>
-    <p>Hello {props.name} you are {props.age} old, you must have been born in {dob()}</p>
-  </div>
-  )
-}
+// const Display = ({ counter }) => <div>{counter}</div>
 
-const Display = ({ counter }) => <div>{counter}</div>
+// const Button = ({button_fn, button_name}) => {
+//   return(
+//   <button onClick = {button_fn}>
+//     {button_name}
+//   </button>
+//   )
+// }
+// const App = (props) => {
+//   const [ counter, setCounter ] = useState(0) //Adds state and rerenders the component. 
+//   //setCounter function is assigned to a function to modify the state.
+//   //When setCounter is called React rerenders the component, i.e gets reexecuted
 
-const Button = ({button_fn, button_name}) => {
-  return(
-  <button onClick = {button_fn}>
-    {button_name}
-  </button>
-  )
-}
-const App = (props) => {
-  const [ counter, setCounter ] = useState(0) //Adds state and rerenders the component. 
-  //setCounter function is assigned to a function to modify the state.
-  //When setCounter is called React rerenders the component, i.e gets reexecuted
-
-  //setTimeout example
-  // const [ counter, setCounter ] = useState(0)
-  // setTimeout(
-  //   () => setCounter(counter + 1),
-  //   1000
-  // )
-  const increment = () => setCounter(counter+1)
-  const reset = () => setCounter(0)
-  const decrement = () => setCounter(counter-1)
+//   //setTimeout example
+//   // const [ counter, setCounter ] = useState(0)
+//   // setTimeout(
+//   //   () => setCounter(counter + 1),
+//   //   1000
+//   // )
+//   const increment = () => setCounter(counter+1)
+//   const reset = () => setCounter(0)
+//   const decrement = () => setCounter(counter-1)
 
 
-  const name = "niranjan"
-  const age = 21
-  return (
-    <div>
-      <Display counter={counter}/>
-      <Hello name = {name} age = {age}/>
-      <p>Implementing a counter</p>
+//   const name = "niranjan"
+//   const age = 21
+//   return (
+//     <div>
+//       <Display counter={counter}/>
+//       <Hello name = {name} age = {age}/>
+//       <p>Implementing a counter</p>
 
-      <div>{counter}</div>
-      <Button button_fn = {increment} button_name = {"plus"}/>
-      <Button button_fn = {reset} button_name = {"reset"} />
-      <Button button_fn = {decrement} button_name = {"minus"} />
+//       <div>{counter}</div>
+//       <Button button_fn = {increment} button_name = {"plus"}/>
+//       <Button button_fn = {reset} button_name = {"reset"} />
+//       <Button button_fn = {decrement} button_name = {"minus"} />
 
       
+//     </div>
+//   )
+// }
+
+
+const App = (props) => {
+  // Using two states
+  // const [left, setLeft] = useState(0)
+  // const [right, setRight] = useState(0)
+
+
+  const [clicks, setClicks] = useState({ left: 0, right: 0})
+  const handleLeftClick = () =>{
+    const newClicks = {
+      ...clicks, left : clicks.left + 1 //Creates a copy of the clicks object, and change value of left
+
+    }
+    setClicks(newClicks)
+  }
+  const handleRightClick = () =>{
+    const newClicks = {
+     ...clicks, right : clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
+
+  return (
+    <div>
+      <div>
+        {clicks.left}
+        <button onClick= {handleLeftClick}> left </button>
+        <button onClick= {handleRightClick}>right</button>
+        {clicks.right}
+      </div>
     </div>
   )
 }
+
+
 
 ReactDOM.render(
   <App />, 
   document.getElementById('root')
 )
+
