c2a18b74080e8670c026f18be68a18929e02ce80
niranjanrao1509 <niranjanrao1509@gmail.com>
2020-05-26 17:54:07 +0400

    part 0 assignments added

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -21,8 +21,6 @@ const Total = ({ parts }) => {
   return <p>Number of exercises {totalExercises}</p>;
 };
 
-
-
 const Content = ({parts}) => {
   return (
   <p>
@@ -47,23 +45,23 @@ const Part = ({ part, exercise }) => {
 
 const App = () => {
   const course = {
-    id: 1,
+    
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10,
-        id: 1
+        
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
-        id: 2
+        
       },
       {
         name: 'State of a component',
         exercises: 14,
-        id: 3
+        
       }
     ]
   }
@@ -77,3 +75,45 @@ const App = () => {
 };
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+
+
+// // import ReactDOM from 'react-dom'
+
+
+// const Header = (props) => {
+//   return(
+//     <div>
+//       <h1>{props.course} </h1>
+//     </div>
+//     )
+// }
+
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+
+//   return (
+//     <div>
+//       <Header course = {course} />
+//       <
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'))
